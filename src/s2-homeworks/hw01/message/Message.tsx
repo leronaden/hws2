692b84205e610e4c1fd7d9409a58e4ad07090d7e
leronaden <lenka200909@gmail.com>
2023-05-05 12:13:32 +0200

    amendmends

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -14,24 +14,29 @@ const Message = (props: MessagePropsType) => {
     return (
         <div id={'hw1-message-1' + props.message.message} className={s.message}>
             <div>
-            <div className={s.imageAndText}>
-                <img src={props.message.user.avatar} alt={'avatar'}
-                     id={'hw1-avatar-1'}/>
-                <div className={s.text}>
+                <div className={s.imageAndText}>
+                    <img src={props.message.user.avatar} alt={'avatar'}
+                         id={'hw1-avatar-1'}/>
 
-                    <div id={'hw1-name-1'} className={s.name}>
-                       <div>{props.message.user.name}</div>
-                    </div>
-                    <pre id={'hw1-text-1'} className={s.messageText}>
+                    <div>
+                        <div className={s.text}>
+
+                            <div id={'hw1-name-1'} className={s.name}>
+                                <div>{props.message.user.name}</div>
+                            </div>
+                            <pre id={'hw1-text-1'} className={s.messageText}>
                            <div>{props.message.message.text}</div>
                     </pre>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
             <div className={s.angle}/>
+            <div>
             <div id={'hw1-time-1'} className={s.time}>
-              <div> {props.message.message.time}</div>
+                <div> {props.message.message.time}</div>
             </div>
+        </div>
         </div>
     )
 }
