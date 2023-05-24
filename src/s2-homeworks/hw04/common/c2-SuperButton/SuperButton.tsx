0642c3e047cbcd9330cfd03b050a7125f9b51350
leronaden <lenka200909@gmail.com>
2023-05-24 20:51:01 +0200

    forth home work - added fixes to supperbutton

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,7 +17,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName =  `${s.button} ${disabled ? s.disabled : xType === 'red' ? s.red : ''} ${className || ''}`;
+    const finalClassName =  `${s.button} ${disabled ? s.disabled : xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : ''} ${className || ''}`;
 
 
     return (
