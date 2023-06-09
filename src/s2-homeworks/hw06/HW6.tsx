e6e40b3e0152d26971cc39039d8a3a415a1f776a
leronaden <lenka200909@gmail.com>
2023-06-09 22:14:12 +0200

    HW6 - logic (button get from is fixes)

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,36 +1,31 @@
-import React, { useState } from 'react'
-import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
-import { restoreState, saveState } from './localStorage/localStorage'
-import s2 from '../../s1-main/App.module.css'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import s from './HW6.module.css'
-
-/*
- * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
- * 2 - дописать логику функции restore
- * 3 - сделать стили в соответствии с дизайном
- */
+import React, { useState } from 'react';
+import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan';
+import { restoreState, saveState } from './localStorage/localStorage';
+import s2 from '../../s1-main/App.module.css';
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton';
+import s from './HW6.module.css';
 
 const HW6 = () => {
-    const [value, setValue] = useState<string>('')
+    const [value, setValue] = useState<string>('');
 
     const save = () => {
-        saveState<string>('hw6-editable-span-value', value)
-    }
+        saveState<string>('hw6-editable-span-value', value);
+    };
+
     const restore = () => {
         const restoredValue = restoreState('hw6-editable-span-value', '');
         setValue(restoredValue);
     };
 
     return (
-        <div id={'hw6'}>
+        <div id="hw6">
             <div className={s2.hwTitle}>Homework #6</div>
 
-            {/*демонстрация возможностей компоненты:*/}
+            {/* демонстрация возможностей компоненты */}
             <div className={s2.hw}>
                 <div className={s.editableSpanContainer}>
                     <SuperEditableSpan
-                        id={'hw6-spanable-input'}
+                        id="hw6-spanable-input"
                         value={value}
                         onChangeText={setValue}
                         spanProps={{
@@ -41,20 +36,17 @@ const HW6 = () => {
                 </div>
 
                 <div className={s.buttonsContainer}>
-                    <SuperButton id={'hw6-save'} onClick={save}>
+                    <SuperButton id="hw6-save" onClick={save}>
                         Save to ls
                     </SuperButton>
-                    <SuperButton
-                        id={'hw6-restore'}
-                        onClick={restore}
-                        xType={'secondary'}
-                    >
+                    <SuperButton id="hw6-restore" onClick={restore} xType="secondary">
                         Get from ls
                     </SuperButton>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default HW6
+export default HW6;
+
